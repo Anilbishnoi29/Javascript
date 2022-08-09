2ff9d43b5b24c91739b3243aacf229bebf89f5f9
Anilbishnoi29 <bishnoianilakb@gmail.com>
2022-08-09 09:42:04 +0530

    js promise

--- part3/asynchronous-javascript/134.js
+++ part3/asynchronous-javascript/134.js
@@ -1,61 +1,65 @@
 // // Promise
 // console.log("script start");
-// const bucket = ['coffee', 'chips','vegetables','salt','rice'];
 
+const heading1 = document.querySelector(".heading1");
+const heading2 = document.querySelector(".heading2");
+const heading3 = document.querySelector(".heading3");
+const heading4 = document.querySelector(".heading4");
+const heading5 = document.querySelector(".heading5");
+const heading6 = document.querySelector(".heading6");
+const heading7 = document.querySelector(".heading7");
+const heading8 = document.querySelector(".heading8");
+const heading9 = document.querySelector(".heading9");
+const heading10 = document.querySelector(".heading10");
 
+// const bucket = ['coffee','chips','vegetables','salt','rice'];
 
-// const friedRicePromise = new Promise((resolve,reject)=>{
-//     if(bucket.includes("vegetables")&& bucket.includes("salt") && bucket.includes("rice")){
-//         resolve({value:"friedrice"});
-//     }else{
-//         reject("could not do it");
+// // create promise
+// const friedRice = new Promise((resolve,reject) => {
+//     if (bucket.includes("rice") && bucket.includes("vegetables")) {
+//         resolve("Resolved -> Done");
+//     } else {
+//         reject("Rejected -> Error");
 //     }
-// })
-
-// // produce 
-
-
-
-// // consume 
-// // how to consume 
-
-// friedRicePromise.then(
-//     // jab promise resolve hoga 
-//     (myfriedRice)=>{
-//     console.log("lets eat ", myfriedRice);
-//     }
-//     ).catch(
-//     (error)=>{
-//         console.log(error)
-//     })
-
-
-// setTimeout(()=>{
-//     console.log("hello from settimeout")
-// },0)
-
-// for(let i = 0; i <=100; i++){
-//     console.log(Math.random(), i);
-// }
-
-// console.log("script end!!!!")
-// setInterval(() => {
-//     console.log((Math.floor(Math.random() * 9)));
 // });
 
-function onSuccess(number1,number2) {
-    console.log(number1 + number2);
-}
-function onFailure() {
-    console.log(`Wrong data Type`);
-}
+// // friedRice.then((resolve) => {
+// //     console.log(resolve);
+// // },(reject) => { console.log(reject); });
 
-function addNumberAndPrint(number1,number2,onSuccess,onFailure) {
-    if (typeof number1 === 'number' && typeof number2 === 'number') {
-        onSuccess(number1,number2);
-    } else {
-        console;
-        onFailure();
-    }
+// friedRice.then(resolve => console.log(resolve)).catch(reject => console.log(reject));
+
+// const obj = {
+//     friedRice: new Promise((resolve,reject) => {
+//         if (bucket.includes("rice") && bucket.includes("vegetables")) {
+//             resolve("Resolved -> Done");
+//         } else {
+//             reject("Rejected -> Error");
+//         }
+//     }),
+// };
+// obj.friedRice.then(value => console.log(value)).catch(value => console.log(value));
+
+// // execute
+function changeText(element,text,color,time) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            if (element) {
+                element.style.color = color;
+                element.textContent = text;
+                resolve();
+            } else {
+                reject();
+            }
+        },time);
+    });
 }
-addNumberAndPrint(2,90090,onSuccess,onFailure);
+changeText(heading1,'one','red',1000).then(() => {
+    return changeText(heading2,'two','pink',1000);
+}).then(() => {
+    return changeText(heading3,'three','yellow',1000);
+}).then(() => {
+    return changeText(heading4,'four','green',2000);
+}).then(() => {
+    return changeText(heading5,'five','brown',1000);
+});;
