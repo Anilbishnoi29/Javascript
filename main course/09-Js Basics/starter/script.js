51bead18d0a15be1f7b543d70517da2bc59386e7
Anilbishnoi29 <bishnoianilakb@gmail.com>
2022-08-14 13:19:23 +0530

    js

--- main course/09-Js Basics/starter/script.js
+++ main course/09-Js Basics/starter/script.js
@@ -1,9 +1,44 @@
 
-let obj = {
-    name: 'orange',
+// let obj = {
+//     name: 'orange',
+// };
+
+// obj = 'red';
+
+
+// console.log(obj);
+
+
+// var n = 2;
+// function square(num) {
+//     var ans = num * num;
+//     return ans;
+// }
+// var square2 = square(n);
+// var square4 = square(4);
+
+// var person = {
+//     first: "Post",
+//     last: "Card"
+// };
+// person = "Bunny";
+
+
+// console.log('1');
+// setTimeout(() => {
+//     console.log('2');
+// });
+// console.log('3');
+
+const list = new Array(60000).join('1.1').split('.');
+
+function removeItemsFromList() {
+    var item = list.pop();
+
+    if (item) {
+        // console.log(item);
+        // setTimeout(removeItemsFromList,1);
+    }
 };
 
-obj = 'red';
-
-
-console.log(obj);
+// removeItemsFromList();
